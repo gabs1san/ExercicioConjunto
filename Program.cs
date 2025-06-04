e7aa4c0487038203928c7d7f4718f6e86c4c2bee
gabs1san <sanbiel398@gmail.com>
2025-06-04 18:27:27 -0300

    Criando a classe onde terá a comparação

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
 
 namespace ExercicioConjunto
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
     }
 }
